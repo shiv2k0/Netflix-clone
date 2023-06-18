3d89d754846fecd821915110998eca9e1d4c9247
Shivendra Singh <shivendrasingh@Shivendras-Mac-mini.local>
2023-06-18 17:01:07 +0530

    Info Modal Component

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 
+import { BiChevronDown } from "react-icons/bi";
 import { BsFillPlayFill } from "react-icons/bs";
 import FavoriteButton from "./FavoriteButton";
 import { useRouter } from "next/router";
+import useInfoModal from "@/hooks/useInfoModel";
 
 interface MovieCardProps {
   data: Record<string, any>;
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
-    const router = useRouter()
+  const router = useRouter();
+  const { openModal } = useInfoModal();
   return (
     <div className="group bg-zinc-900 col-span-1 relative h-[12vw]">
       <img
@@ -28,12 +31,15 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
             <div
               className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300"
               onClick={() => {
-                router.push(`/watch/${data?.id}`)
+                router.push(`/watch/${data?.id}`);
               }}
             >
               <BsFillPlayFill size={30} />
             </div>
             <FavoriteButton movieId={data?.id} />
+            <div onClick={()=>openModal(data?.id)} className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300 ">
+              <BiChevronDown size={30} className="text-white group-hover/item:text-neutral-300 "/>
+            </div>
           </div>
           <p className="text-green-400 font-semibold mt-4">
             New <span className="text-white"> 2023</span>
